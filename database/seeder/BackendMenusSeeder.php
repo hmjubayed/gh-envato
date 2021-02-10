f8d4570a8d0698db327b7127f860a480a0ee95e8
Jubayed Bhuiyan <70032786+jubayed-me@users.noreply.github.com>
2021-02-11 02:12:27 +0600

    Update BackendMenusSeeder.php

--- database/seeder/BackendMenusSeeder.php
+++ database/seeder/BackendMenusSeeder.php
@@ -16,36 +16,6 @@ class BackendMenusSeeder extends Seeder
     public function run()
     {
 
-
-        $menu = Menu::where("name", "main")->firstOrNew();
-        $menuItem = MenuItem::firstOrNew([
-            "menu_id" => $menu->id,
-            "title"   => "menus.backend.main.pages",
-            "route"    => 'backend.pages.index',
-        ]);
-        if (!$menuItem->exists) {
-            $menuItem->fill([
-                "target"     => "_self",
-                "icon" => "file-text",
-                "parent_id"  => 0,
-                "order"      => 1,
-            ])->save();
-        }
-
-        $menuItem = MenuItem::firstOrNew([
-            "menu_id" => $menu->id,
-            "title"   => "menus.backend.main.posts",
-            "route"    => 'backend.posts.index',
-        ]);
-        if (!$menuItem->exists) {
-            $menuItem->fill([
-                "target"     => "_self",
-                "icon" => "file",
-                "parent_id"  => 0,
-                "order"      => 1,
-            ])->save();
-        }
-
         $menuItem = MenuItem::firstOrNew([
             "menu_id" => $menu->id,
             "title"   => "menus.backend.main.gh-envato",
@@ -60,7 +30,5 @@ class BackendMenusSeeder extends Seeder
             ])->save();
         }
 
-        // site menus
-        
     }
 }
